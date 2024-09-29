918b1e5a50d66fa19fc6bafa45157828dfb67654
invicinity <invicinityofsorrow@gmail.com>
2024-09-29 22:56:38 +0200

    Update settings.js

--- js/config/settings.js
+++ js/config/settings.js
@@ -155,11 +155,11 @@ export function applyTheme(theme) {
     inputPrefix.style.color = 'rgb(62, 209, 46)';
     document.documentElement.style.setProperty('--cursor-color', 'rgb(62, 209, 46)');
   } else if (theme === 'Orange') {
-    terminal.style.background = 'hsla(30, 57%, 14%, 1)';
+    terminal.style.background = 'hsla(0, 0%, 100%, 1)';
     terminal.style.backgroundImage = 'radial-gradient(circle, hsla(30, 57%, 14%, 1) 0%, hsla(30, 67%, 5%, 1) 100%)';
-    terminalInput.style.color = '#FFA128';
-    terminalOutput.style.color = '#FFA128';
-    inputPrefix.style.color = '#FFA128';
+    terminalInput.style.color = '#FFFFFF';
+    terminalOutput.style.color = '#FFFFFF';
+    inputPrefix.style.color = '#FFFFFF';
     document.documentElement.style.setProperty('--cursor-color', '#FFA128');
   }
 }
